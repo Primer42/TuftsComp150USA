b73c87ea4b017a631c22b9c9ff0b21bc1aa04250
Will Richard <willster3021@gmail.com>
2013-06-06 07:42:59 -0700

    Added extra credit functionality

--- lab1/myps.sh
+++ lab1/myps.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-ps -ef | grep "^wrichard"
+if [ $# -gt 1 ]
+then
+    printf "Usage: $0 [user name]\n"
+    exit 1
+fi
+
+if [ $# -eq 1 ]
+then
+    USER="$1"
+fi
+
+ps -ef | grep "^$USER" | awk '{print $1 " " $8}'
