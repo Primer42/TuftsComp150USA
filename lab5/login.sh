75adac405b81c50a2c9519baa718151ffa9566fe
Will Richard <willster3021@gmail.com>
2013-06-22 09:17:44 -0700

    Completed login.sh script.

---
+++ lab5/login.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+fullname=`grep $USERNAME /etc/passwd | cut -d: -f5`;
+time=`date "+%l:%M%P" | sed -e 's/^ *//g'`;
+procinfo=`grep "model name" /proc/cpuinfo | cut -d: -f2 | sed -e 's/^ *//g'`;
+meminfo=`cat /proc/meminfo | grep "MemTotal" | cut -d: -f2 | sed -e 's/^ *//g'`;
+
+echo "$procinfo";
+echo "Welcome to $HOSTNAME, $fullname!";
+echo "You are logged in as $USERNAME and your current directory is $PWD";
+echo "The time is $time";
+echo "System Processor: $procinfo";
+echo "Sytem Memory: $meminfo";
