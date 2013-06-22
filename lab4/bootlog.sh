f0c1aa732dc7bc1a9537cb0b9d53adb9bf89c47c
Will Richard <willster3021@gmail.com>
2013-06-22 12:24:13 -0700

    Adding extra credit bootlog assignment.

--- lab4/bootlog.sh
+++ lab4/bootlog.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+#
+# chkconfig: 345 98 2
+# description: This script writes a log message when the computer is booted or shut down.
 
-echo `date` $HOSTNAME "booted!" >> /home/wrichard/boot.log
+usagestr="Usage $0 [start|stop]";
+
+if [ $# -ne 1 ] 
+then
+    echo "$usagestr";
+    exit 1
+elif [ "$1" = "start" ]
+then
+    echo `date` $HOSTNAME "booted!" >> /home/wrichard/boot.log
+elif [ "$1" = "stop" ]
+then
+    echo `date` $HOSTNAME "shutting down" >> /home/wrichard/boot.log
+else
+    echo "$usagestr";
+    exit 1
+fi
